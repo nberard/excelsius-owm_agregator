8902d64d120eca212773770626a3a001053bba11
Nicolas BERARD <n.berard@meetic-corp.com>
2016-03-10 11:15:36 +0100

    feat: use external config

--- collect.py
+++ collect.py
@@ -2,25 +2,44 @@
 # -*-coding:utf-8 -*
 
 import requests
-import json
+import os
+import sys
+import yaml
+import datetime
+from elasticsearch import Elasticsearch
+
 from urllib import parse
+
+base_dir = os.path.dirname(__file__)
 
 endpoint = "http://api.openweathermap.org/data/2.5/"
 uri = "group"
-cities_file = "data/cities.json"
 units = "metric"
-token = "101f79b79f822b47d18d196e900a0601"
+config = yaml.safe_load(open(os.path.join(base_dir, "config.yml")))
+token = config["open_weather_map"]["token"]
 
-with open(cities_file) as cities_content:
-    cities_objects = json.load(cities_content)
-    ids = list(map(lambda x: str(x["id"]), cities_objects))
-    print(ids)
+ids = list(map(lambda x: str(x["id"]), config["cities"]))
 
 parameters_string = parse.urlencode({"id": ",".join(ids), "units": units, "APPID": token})
 url = endpoint + uri + "?" + parameters_string
 
 temperatures_fetched = requests.get(url).json()
 temperatures_to_index = {}
+connect_string = ":".join([config["elastic_search"]["host"], str(config["elastic_search"]["port"])])
+es = Elasticsearch(connect_string)
+now_date = datetime.datetime.now().isoformat()
 for city_temperature in temperatures_fetched["list"]:
-    temperatures_to_index[city_temperature["name"]] = city_temperature["main"]["temp"]
-print(temperatures_to_index)
+    body = {
+        "date": now_date,
+        "temperature": city_temperature["main"]["temp"],
+        "owm_id": city_temperature["id"],
+        "name": city_temperature["name"],
+        "long": city_temperature["coord"]["lon"],
+        "lat": city_temperature["coord"]["lat"],
+        "humidity": city_temperature["main"]["humidity"]
+    }
+    response = es.create(index="excelsius", doc_type="temperature", body=body)
+    if not response["created"]:
+        print("unable to create: ", body, file=sys.stderr)
+    else:
+        print("successfully created: ", body)
